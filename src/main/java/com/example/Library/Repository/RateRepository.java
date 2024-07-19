a53e0cc301a32e874e04db3bf0da90ead211378e
Sepehr236 <67850396+Sepehr236@users.noreply.github.com>
2024-07-19 16:44:10 +0330

    RateRepository was updated to count number of diffrent Rates

--- src/main/java/com/example/Library/Repository/RateRepository.java
+++ src/main/java/com/example/Library/Repository/RateRepository.java
@@ -2,8 +2,26 @@ package com.example.Library.Repository;
 
 import com.example.Library.Model.Rate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RateRepository extends JpaRepository<Long, Rate> {
+public interface RateRepository extends JpaRepository<Rate, Long> {
+    @Query(value = "SELECT COUNT(r) FROM Rate r")
+    Integer numberOfRates();
+
+    @Query(value = "SELECT COUNT(r) FROM Rate r WHERE r.rate = 5")
+    Integer fiveStars();
+
+    @Query(value = "SELECT COUNT(r) FROM Rate r WHERE r.rate = 4")
+    Integer fourStars();
+
+    @Query(value = "SELECT COUNT(r) FROM Rate r WHERE r.rate = 3")
+    Integer threeStars();
+
+    @Query(value = "SELECT COUNT(r) FROM Rate r WHERE r.rate = 2")
+    Integer twoStars();
+
+    @Query(value = "SELECT COUNT(r) FROM Rate r WHERE r.rate = 1")
+    Integer oneStars();
 }
