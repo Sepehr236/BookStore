125b07f5def23cda1d176ac257a282b0e2e80caf
Sepehr236 <67850396+Sepehr236@users.noreply.github.com>
2024-07-19 16:56:09 +0330

    RateRepository was updated so that we can count each rate

--- src/main/java/com/example/Library/Repository/RateRepository.java
+++ src/main/java/com/example/Library/Repository/RateRepository.java
@@ -7,4 +7,21 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RateRepository extends JpaRepository<Rate, Long> {
+    @Query(value = "SELECT COUNT(r) FROM Rate r")
+    Integer numberOfRates();
+
+    @Query(value = "SELECT COUNT(r) FROM Rate r WHERE r.rate = 5")
+    Integer fiveStars();
+
+    @Query(value = "SELECT COUNT(r) FROM Rate r WHERE r.rate = 4")
+    Integer fourStars();
+
+    @Query(value = "SELECT COUNT(r) FROM Rate r WHERE r.rate = 3")
+    Integer threeStars();
+
+    @Query(value = "SELECT COUNT(r) FROM Rate r WHERE r.rate = 2")
+    Integer twoStars();
+
+    @Query(value = "SELECT COUNT(r) FROM Rate r WHERE r.rate = 1")
+    Integer oneStars();
 }
