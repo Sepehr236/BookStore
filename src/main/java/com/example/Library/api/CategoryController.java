f4af6a89b99d7086393bdcb991cb5f29b1ed1e60
Sepehr236 <67850396+Sepehr236@users.noreply.github.com>
2024-07-24 09:22:30 +0330

    getBookBycategory method is added to CategoryController.

--- src/main/java/com/example/Library/api/CategoryController.java
+++ src/main/java/com/example/Library/api/CategoryController.java
@@ -1,10 +1,14 @@
 package com.example.Library.api;
 
+import com.example.Library.model.Book;
 import com.example.Library.model.Category;
 import com.example.Library.service.CategoryService;
+import com.example.Library.service.CommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/Category")
@@ -15,6 +19,11 @@ public class CategoryController {
     @PostMapping
     public ResponseEntity<Category> addCategory(Category category){
         return ResponseEntity.ok(categoryService.addCategory(category));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<List<Book>> getBookByCategory(@PathVariable("id") Long id){
+        return ResponseEntity.ok(categoryService.getBookByCategory(id));
     }
 
     @DeleteMapping("/{id}")
