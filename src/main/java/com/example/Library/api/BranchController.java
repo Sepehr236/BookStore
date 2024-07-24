78ffa8c878e986362bd698618d3d7e6c5a94d72e
Sepehr236 <67850396+Sepehr236@users.noreply.github.com>
2024-07-24 10:08:19 +0330

    BranchController is added.

---
+++ src/main/java/com/example/Library/api/BranchController.java
@@ -0,0 +1,35 @@
+package com.example.Library.api;
+
+import com.example.Library.model.Book;
+import com.example.Library.model.CategoryBranch;
+import com.example.Library.repository.CategoryBranchRepository;
+import com.example.Library.service.CategoryBranchService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("api/Branch")
+@RestController
+@RequiredArgsConstructor
+public class BranchController {
+    private final CategoryBranchService categoryBranchService;
+
+    @PostMapping
+    public ResponseEntity<CategoryBranch> addBranch(CategoryBranch categoryBranch){
+        return ResponseEntity.ok(categoryBranchService.addBranch(categoryBranch));
+    }
+
+    @GetMapping("/{branchId}")
+    public ResponseEntity<List<Book>> addBookToBranch(@PathVariable("branchId") Long branchId
+            , @RequestBody Long bookId){
+
+        return ResponseEntity.ok(categoryBranchService.addBookToBranch(branchId, bookId));
+    }
+
+    @DeleteMapping("/{branchId}")
+    public void deleteBookFromBranch(@PathVariable("branchId") Long branchId, @RequestBody Long bookId){
+        categoryBranchService.deleteBookFromBranch(branchId, bookId);
+    }
+}
