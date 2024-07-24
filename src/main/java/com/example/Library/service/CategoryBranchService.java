769dee87589a8aa1a6cbc5ce827f0502e83b926c
Sepehr236 <67850396+Sepehr236@users.noreply.github.com>
2024-07-24 09:51:14 +0330

    CategoryBranchService is created and addBookToBranch and addCategory methods are added to it.

---
+++ src/main/java/com/example/Library/service/CategoryBranchService.java
@@ -0,0 +1,36 @@
+package com.example.Library.service;
+
+import com.example.Library.exeption.ResourceNotFound;
+import com.example.Library.model.Book;
+import com.example.Library.model.CategoryBranch;
+import com.example.Library.repository.BookRepository;
+import com.example.Library.repository.CategoryBranchRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class CategoryBranchService {
+    private final CategoryBranchRepository categoryBranchRepository;
+    private final BookRepository bookRepository;
+
+    public CategoryBranch addBranch(CategoryBranch categoryBranch){
+        return categoryBranchRepository.save(CategoryBranch.builder()
+                        .branchName(categoryBranch.getBranchName())
+                .build());
+    }
+
+    public List<Book> addBookToBranch(Long branchId, Long bookId){
+        CategoryBranch categoryBranch = categoryBranchRepository.findById(branchId)
+                .orElseThrow(ResourceNotFound.instance("Branch not found !!!"));
+        Book book = bookRepository.findById(bookId)
+                .orElseThrow(ResourceNotFound.instance("Book not found !!!"));
+
+        categoryBranch.getBooks().add(book);
+        return categoryBranch.getBooks();
+    }
+}
