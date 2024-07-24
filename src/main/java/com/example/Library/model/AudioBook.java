0715f8da9492f95ec757161ba07ea414377103d9
Sepehr236 <67850396+Sepehr236@users.noreply.github.com>
2024-07-24 07:45:26 +0330

    Book has become a SuperBuilder and AudioBook is now extending Book.

---
+++ src/main/java/com/example/Library/model/AudioBook.java
@@ -0,0 +1,19 @@
+package com.example.Library.model;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@Entity
+public class AudioBook extends Book{
+    private Long narrator;
+    private String narratingTime;
+}
--- src/main/java/com/example/Library/model/Book.java
+++ src/main/java/com/example/Library/model/Book.java
@@ -2,6 +2,7 @@ package com.example.Library.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 import java.util.List;
 
@@ -9,7 +10,7 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
+@SuperBuilder
 @Entity
 public class Book {
     @Id
